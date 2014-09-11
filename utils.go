bb4630f69b37f196c4cbca9cf763a23bcfa5ec7c
tonic <tonicbupt@gmail.com>
2014-09-11 16:01:16 +0800

    remove unused code

--- utils.go
+++ utils.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"os/user"
 	"strconv"
 )
 
@@ -30,44 +29,12 @@ func YAMLEncode(v interface{}) (string, error) {
 	return string(r), err
 }
 
-func GetUid(username string) (string, error) {
-	user, err := user.Lookup(username)
-	if err != nil {
-		return "", err
-	}
-	return user.Uid, nil
-}
-
-func GetGid(username string) (string, error) {
-	user, err := user.Lookup(username)
-	if err != nil {
-		return "", err
-	}
-	return user.Gid, nil
-}
-
 func EnsureDir(path string, owner, group int) error {
 	err := os.Mkdir(path, 0755)
 	if err != nil {
 		return err
 	}
 	return os.Chown(path, owner, group)
-}
-
-func EnsureFile(path string, owner, group int, content []byte) error {
-	file, err := os.Create(path)
-	defer file.Close()
-	if err != nil {
-		return nil
-	}
-	file.Write(content)
-	os.Chmod(path, 0755)
-	os.Chown(path, owner, group)
-	return nil
-}
-
-func EnsureDirAbsent(path string) error {
-	return os.RemoveAll(path)
 }
 
 func EnsureFileAbsent(path string) error {
