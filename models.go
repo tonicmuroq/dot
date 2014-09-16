606bc168e97d2c13ecb73017879221840db993fa
tonic <tonicbupt@gmail.com>
2014-09-16 13:59:04 +0800

    remove testing code

--- models.go
+++ models.go
@@ -82,11 +82,6 @@ func init() {
 
 	// Mutex
 	portMutex = sync.Mutex{}
-
-	// for testing
-	NewApplication("blueberry", "11111", `{"appname": "blurry", "runtime": "python", "port": 5000, "cmd": ["pip install"], "services": ["mysql", "redis"]}`, "")
-	NewApplication("nbetest", "4f1c7c2", `{"appname": "nbetest", "runtime": "python", "port": 5000, "cmd": ["python app.py"], "services": ["mysql", "redis"]}`, "")
-	NewHost("127.0.0.1", "tonic")
 }
 
 // Application
