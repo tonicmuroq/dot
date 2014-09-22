1b55a57f49dcd6db43e6f4a0742df212e8353efd
tonic <tonicbupt@gmail.com>
2014-09-22 09:15:18 +0800

    forget prefix

--- models.go
+++ models.go
@@ -223,7 +223,7 @@ func (self *Application) GetOrCreateDbInfo() string {
 func (self *Application) CreateDNS() error {
 	dns := make(map[string]string)
 	dns["host"] = config.Masteraddr
-	cpath := fmt.Sprintf("/com/hunantv/intra/%s", self.Name)
+	cpath := fmt.Sprintf("/skydns/com/hunantv/intra/%s", self.Name)
 	_, err := etcdClient.Create(cpath, "", 0)
 	if err != nil {
 		return err
