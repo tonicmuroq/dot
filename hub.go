dd205528e6cce72c8ef0efb2489d5ddf3d89c39f
tonic <tonicbupt@gmail.com>
2014-11-07 12:03:48 +0800

    fix websocket

--- hub.go
+++ hub.go
@@ -27,6 +27,7 @@ const (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024 * 1024,
 	WriteBufferSize: 1024 * 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
 var ZeroTime time.Time
