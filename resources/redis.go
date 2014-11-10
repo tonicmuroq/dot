d776f0da76e8fab53102160058b38b03d58e1dd6
tonic <tonicbupt@gmail.com>
2014-11-10 16:48:06 +0800

    add redis/mysql

--- resources/redis.go
+++ resources/redis.go
@@ -1,12 +1,35 @@
 package resources
 
 import (
-	"time"
+	"../config"
+	"fmt"
+	"net/url"
+	"path"
+	"strconv"
 )
 
 func NewRedisInstance(appname string) (map[string]interface{}, error) {
+	u := path.Join(config.Config.Redismgr, fmt.Sprintf("/start/%s", appname))
+	ret, err := Post(u, url.Values{})
+	if err != nil {
+		return nil, err
+	}
+	port, _ := strconv.Atoi(ret["port"])
 	return map[string]interface{}{
-		"host": "10.1.201.88",
-		"port": time.Now().Nanosecond()%13 + 2000,
+		"host": ret["host"],
+		"port": port,
+	}, nil
+}
+
+func ExpandRedisInstance(appname string) (map[string]interface{}, error) {
+	u := path.Join(config.Config.Redismgr, fmt.Sprintf("/expand/%s", appname))
+	ret, err := Post(u, url.Values{})
+	if err != nil {
+		return nil, err
+	}
+	port, _ := strconv.Atoi(ret["port"])
+	return map[string]interface{}{
+		"host": ret["host"],
+		"port": port,
 	}, nil
 }
