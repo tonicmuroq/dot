16b1461f50e9498950dcfce15d5244ae279c5903
tonic <tonicbupt@gmail.com>
2014-12-08 11:54:40 +0800

    add one column

--- hub.go
+++ hub.go
@@ -59,6 +59,9 @@ func (self *Hub) CheckAlive() {
 			// 如果一个连接不再存在, 那么删掉这个连接
 			if duration.Seconds() > float64(checkAliveDuration) {
 				Logger.Info(host, " is disconnected.")
+				if h := models.GetHostByIP(host); h != nil {
+					h.Offline()
+				}
 				self.RemoveLevi(host)
 			}
 		}
