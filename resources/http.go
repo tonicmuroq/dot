85de3061426a169f64157ec72fdfe12496bc4f07
tonic <tonicbupt@gmail.com>
2015-01-04 11:56:35 +0800

    use get

--- resources/http.go
+++ resources/http.go
@@ -24,3 +24,21 @@ func Post(addr string, form url.Values) (map[string]interface{}, error) {
 	}
 	return data, nil
 }
+
+func Get(addr string) (map[string]interface{}, error) {
+	r, err := http.Get(addr)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+	content, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+	var data map[string]interface{}
+	err = json.Unmarshal(content, &data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
